ce9c59ca7fda729f758316b175bce696def68134
Ehren Davis <ehren.davis12@gmail.com>
2020-01-30 21:11:26 -0700

    use the python API to get data. and fix .env file

--- src/shared/data.js
+++ src/shared/data.js
@@ -1,4 +1,4 @@
-import * as axios from "../../node_modules/axios";
+// import * as axios from "../../node_modules/axios";
 
 import { format } from "date-fns";
 import { displayDateTimeFormat } from "./constants";
@@ -7,12 +7,11 @@ import { API } from "./config";
 
 const getUsers = async function() {
   try {
-    const response = await axios.get(`${API}/users.json`);
-    let data = parseList(response);
-
+    // const response = await axios.get(`${API}/users.json`);
+    const response = await fetch(`${API}/app-users-all`);
+    let data = await parseListFetch(response);
     const users = data.map(user => {
       user.last_login = new Date(user.last_login);
-      // user.last_login = format(user.last_login, "MM/dd/yyyy");
       user.last_login = format(user.last_login, displayDateTimeFormat);
       return user;
     });
@@ -25,15 +24,25 @@ const getUsers = async function() {
   }
 };
 
-const parseList = response => {
+const parseListFetch = async function(response) {
   if (response.status !== 200) throw Error(response.message);
-  if (!response.data) return [];
-  let list = response.data;
-  if (typeof list !== "object") {
+  if (!response.body) return [];
+  let list = await response.text();
+  if (list === "") {
     list = [];
   }
-  return list;
+  return JSON.parse(list);
 };
+
+// const parseListAxios = response => {
+//   if (response.status !== 200) throw Error(response.message);
+//   if (!response.data) return [];
+//   let list = response.data;
+//   if (typeof list !== "object") {
+//     list = [];
+//   }
+//   return list;
+// };
 
 export const data = {
   getUsers
