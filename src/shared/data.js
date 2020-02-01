6c997cfc419ee2c7668b887e7326ec433398dd28
Ehren Davis <ehren.davis12@gmail.com>
2020-01-31 19:53:10 -0700

    lets try axios again

--- src/shared/data.js
+++ src/shared/data.js
@@ -1,4 +1,6 @@
 // import * as axios from "../../node_modules/axios";
+import axios from "axios";
+axios.defaults.crossDomain = true;
 
 import { format } from "date-fns";
 import { displayDateTimeFormat } from "./constants";
@@ -8,14 +10,10 @@ import { API } from "./config";
 const getUsers = async function() {
   try {
     // const response = await axios.get(`${API}/users.json`);
-    const response = await fetch(`${API}/app-users-all`, {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-        "Access-Control-Allow-Origin": "*"
-      }
-    });
-    let data = await parseListFetch(response);
+    const response = await axios.get(`${API}/app-users-all`);
+    // const response = await fetch(`${API}/app-users-all`);
+    const data = parseListAxios(response);
+    // const data = await parseListFetch(response);
     const users = data.map(user => {
       user.last_login = new Date(user.last_login);
       user.last_login = format(user.last_login, displayDateTimeFormat);
@@ -30,25 +28,25 @@ const getUsers = async function() {
   }
 };
 
-const parseListFetch = async function(response) {
-  if (response.status !== 200) throw Error(response.message);
-  if (!response.body) return [];
-  let list = await response.text();
-  if (list === "") {
-    list = [];
-  }
-  return JSON.parse(list);
-};
-
-// const parseListAxios = response => {
+// const parseListFetch = async function(response) {
 //   if (response.status !== 200) throw Error(response.message);
-//   if (!response.data) return [];
-//   let list = response.data;
-//   if (typeof list !== "object") {
+//   if (!response.body) return [];
+//   let list = await response.text();
+//   if (list === "") {
 //     list = [];
 //   }
-//   return list;
+//   return JSON.parse(list);
 // };
+
+const parseListAxios = response => {
+  if (response.status !== 200) throw Error(response.message);
+  if (!response.data) return [];
+  let list = response.data;
+  if (typeof list !== "object") {
+    list = [];
+  }
+  return list;
+};
 
 export const data = {
   getUsers
